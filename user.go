0bde04a375a7c35d852d7cfbafe1a0b8982ec60f
six <scirocco6@gmail.com>
2015-07-07 16:28:03 -0700

    refactoriing.  Unsure if this is really any better

--- user.go
+++ user.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"errors"
 	"net"
 	"os"
 	"strings"
@@ -23,7 +23,9 @@ func ReadFromUser(connection net.Conn) {
 			continue
 		}
 
-		var packet icb.Packet
+		var packet *icb.Packet
+		var err error
+
 		if strings.HasPrefix(message, "/") { // handle commands
 			message = message[1:]
 			command := strings.SplitN(message, " ", 3)
@@ -31,49 +33,68 @@ func ReadFromUser(connection net.Conn) {
 			switch command[0] {
 			case "beep":
 				{
-					if len(command) == 2 {
-						packet = icb.CreatePacket("beep", command[1])
-					} else {
-						PrintToScreen("Usage: /beep nick")
-					}
+					packet, err = beep(command)
 				}
 			case "m": // send a private message to a user
 				{
-					if len(command) == 3 {
-						packet = icb.CreatePacket("private", command[1], command[2])
-					} else {
-						PrintToScreen("Usage: /m nick message")
-					}
+					packet, err = privateMessage(command)
 				}
 			case "w": // obtain a listing of who is on
 				{
-					if len(command) == 1 { // obtain listing of who is in every group
-						packet = icb.CreatePacket("global_who")
-					} else { // listing for who is in a particular group or group a user is in
-						packet = icb.CreatePacket("local_who", command[1])
-					}
+					packet = globalWho(command)
 				}
 			case "g": // join a group
 				{
-					if len(command) == 1 {
-						packet = icb.CreatePacket("join", command[1])
-					} else {
-						PrintToScreen("Usage: /g group")
-					}
+					packet, err = join(command)
 				}
-			case "q":
+			case "q": // quit
 				{
 					cleanExit()
 				}
 			default:
 				{
-					fmt.Printf("Unrecognized command \n'/%s'\n", message)
-					continue
+					err = errors.New("Unrecognized command \n'/" + message + "'\n")
 				}
 			}
 		} else { // by defualt send a public message to the channel
 			packet = icb.CreatePacket("public", message)
 		}
-		packet.SendTo(connection)
+		if err == nil && packet != nil {
+			packet.SendTo(connection)
+		} else if err != nil {
+			PrintToScreen(err.Error())
+		}
 	}
+}
+
+func beep(parameters []string) (*icb.Packet, error) {
+	if len(parameters) != 2 {
+		return nil, errors.New("Usage: /beep nick")
+	}
+
+	return icb.CreatePacket("beep", parameters[1]), nil
+}
+
+func privateMessage(parameters []string) (*icb.Packet, error) {
+	if len(parameters) != 3 {
+		return nil, errors.New("Usage: /m nick message")
+	}
+
+	return icb.CreatePacket("private", parameters[1], parameters[2]), nil
+}
+
+func globalWho(parameters []string) *icb.Packet {
+	if len(parameters) == 1 { // obtain listing of who is in every group
+		return icb.CreatePacket("global_who")
+	}
+	// listing for who is in a particular group or group a user is in
+	return icb.CreatePacket("local_who", parameters[1])
+}
+
+func join(parameters []string) (*icb.Packet, error) {
+	if len(parameters) != 1 {
+		return nil, errors.New("Usage: /g group")
+	}
+
+	return icb.CreatePacket("join", parameters[1]), nil
 }
